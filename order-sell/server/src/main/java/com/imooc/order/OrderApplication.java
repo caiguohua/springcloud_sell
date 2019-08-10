81ed0bfb6c48b98753fcf0be3676717f251c6f3b
caiguohua <cai.beyond@qq.com>
2019-08-10 10:33:22 +0800

    order

---
+++ order-sell/server/src/main/java/com/imooc/order/OrderApplication.java
@@ -0,0 +1,23 @@
+package com.imooc.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+//import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients(basePackages = "com.imooc.product.client")
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
+@SpringCloudApplication
+@ComponentScan(basePackages = "com.imooc")
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+}
